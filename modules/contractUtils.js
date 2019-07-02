4a1eb0716c94a10581122205ae0a7821012038ea
smitrajput <smitrajputtd@gmail.com>
2019-07-02 23:38:07 +0530

    Add transaction flag

--- modules/contractUtils.js
+++ modules/contractUtils.js
@@ -26,4 +26,21 @@ contract.getDetails = async (base_url, address, spinner) => {
     }
 }
 
+contract.getTransactions = async (base_url, address, spinner) => {
+    spinner.start();
+    const data = await fetch(base_url+`/contracts/${address}/transactions`);
+    let jsonData = await data.json();
+    // if(jsonData["data"]["relationships"]["token"]["data"] != null){
+    //     tokenData = await fetch(base_url + "/tokens/" + jsonData["data"]["relationships"]["token"]["data"]["id"])
+    //     tokenDataJSON = await tokenData.json();
+    // }
+    spinner.stop();
+    // console.log("Address:\t\t", jsonData["data"]["attributes"]["address"]);
+    // console.log("Balance:\t\t", jsonData["data"]["attributes"]["balance"]);
+    // console.log("Creation Time:\t\t", moment.unix(parseInt(jsonData["data"]["attributes"]["createdAtTimestamp"])).local().toString());
+    // console.log("Constructor Arguments:\t", jsonData["data"]["attributes"]["constructorArgs"])
+    console.log(jsonData);
+}
+
+
 module.exports = contract;
